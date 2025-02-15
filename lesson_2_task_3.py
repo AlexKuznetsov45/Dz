501650b07158dd20f218e6b386444671e880dca0
AlexKuznetsov45 <alekseykuznetsov2024@gmail.com>
2025-02-15 13:21:08 +0300

    Delete lesson_2_task_3.py

--- lesson_2_task_3.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def square(side_length):
-    area = side_length ** 2
-    if not isinstance(side_length, int):
-        return math.ceil(area)
-    else:
-        return area
-print(square(5))
