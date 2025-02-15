9ca2f26a4b32c3fc98b9e5f599bcb9433c302ceb
AlexKuznetsov45 <alekseykuznetsov2024@gmail.com>
2025-02-15 13:20:58 +0300

    Delete lesson_2_task_2.py

--- lesson_2_task_2.py
+++
@@ -1,8 +0,0 @@
-def is_year_leap(year):
-    if year % 4 == 0:
-        return True
-    else:
-        return False
-test_year = 2024
-leap_year_result = is_year_leap(test_year)
-print(f'Год {test_year}: {leap_year_result}')
